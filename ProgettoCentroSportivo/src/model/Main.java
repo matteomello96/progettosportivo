68d0e99f704b21f7fe9b7c0a2216b29203c907e8
Matteo <Matteo@Matteo-PC>
2017-04-05 14:26:35 +0200

    hhiih

---
+++ ProgettoCentroSportivo/src/model/Main.java
@@ -0,0 +1,13 @@
+package model;
+
+import en.progetto.view.admin_dev.AdminFrame;
+import it.progetto.view.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SetLook.setLooksys();
+		new LoginFrame();
+		
+	}
+}
