bd5c51af1edc24d08559c3a3e2e8339e78ff7040
Claudio <Claudio@claudio>
2017-05-30 17:43:50 +0200

    vdfddsf

--- ProgettoCentroSportivo/src/DBInterfaccia/DbConnection.java
+++ ProgettoCentroSportivo/src/DBInterfaccia/DbConnection.java
@@ -12,7 +12,7 @@ public class DbConnection {
 		   if(instance == null)
 			   instance = new DbConnection();
 		   if(connesso != true)
-				connetti("centropolisportivo", "root", "Bla.bla.12");
+				connetti("centropolisportivo", "root", "Furfudopfa81");
 		   return instance;
 	   }
 	   
