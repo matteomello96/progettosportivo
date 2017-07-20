6cc582a4cb428299b4a3b9b51ab90067a60112f5
Matteo <Matteo@Matteo-PC>
2017-07-20 16:45:25 +0200

    Matteo 20/07/2017

--- ProgettoCentroSportivo/src/VisteUtenteGenerico/FrameRegistrazione.java
+++ ProgettoCentroSportivo/src/VisteUtenteGenerico/FrameRegistrazione.java
@@ -104,17 +104,15 @@ public class FrameRegistrazione extends JFrame {
 		
 		
 	
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		
- 
+		
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setBackground(new Color (0,220,50));
-		  
+		contentPane.setBackground(new Color (64,224,208));
+		contentPane.setLayout(new GridBagLayout());
+		 
+		 GridBagConstraints gbc = new GridBagConstraints();
+ 
 		JScrollPane scroll = new JScrollPane(contentPane);
 		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -138,248 +136,202 @@ public class FrameRegistrazione extends JFrame {
 		
 		
 		JLabel lblFormDiRegistrazione = new JLabel("Form di registrazione");
-		GridBagConstraints gbc_lblFormDiRegistrazione = new GridBagConstraints();
-		gbc_lblFormDiRegistrazione.gridwidth = 2;
-		gbc_lblFormDiRegistrazione.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFormDiRegistrazione.gridx = 4;
-		gbc_lblFormDiRegistrazione.gridy = 0;
-		contentPane.add(lblFormDiRegistrazione, gbc_lblFormDiRegistrazione);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.LINE_END;
+		contentPane.add(lblFormDiRegistrazione, gbc);
 		
 		JLabel lblDatiAnagrafici = new JLabel("Dati Anagrafici");
-		GridBagConstraints gbc_lblDatiAnagrafici = new GridBagConstraints();
-		gbc_lblDatiAnagrafici.gridwidth = 2;
-		gbc_lblDatiAnagrafici.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDatiAnagrafici.gridx = 4;
-		gbc_lblDatiAnagrafici.gridy = 1;
-		contentPane.add(lblDatiAnagrafici, gbc_lblDatiAnagrafici);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.LINE_END;
+		contentPane.add(lblDatiAnagrafici, gbc);
 		
 		
 		
 		JLabel lblNome = new JLabel("Nome");
-		GridBagConstraints gbc_lblNome = new GridBagConstraints();
-		gbc_lblNome.anchor = GridBagConstraints.EAST;
-		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNome.gridx = 3;
-		gbc_lblNome.gridy = 2;
-		contentPane.add(lblNome, gbc_lblNome);
+		gbc.anchor =  GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		contentPane.add(lblNome, gbc);
 		
 		textnome = new JTextField();
-		GridBagConstraints gbc_textnome = new GridBagConstraints();
-		gbc_textnome.gridwidth = 2;
-		gbc_textnome.insets = new Insets(0, 0, 5, 5);
-		gbc_textnome.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textnome.gridx = 4;
-		gbc_textnome.gridy = 2;
-		contentPane.add(textnome, gbc_textnome);
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		contentPane.add(textnome, gbc);
 		textnome.setColumns(10);
 		
 		JLabel lblCognome = new JLabel("Cognome");
 		GridBagConstraints gbc_lblCognome = new GridBagConstraints();
-		gbc_lblCognome.anchor = GridBagConstraints.EAST;
-		gbc_lblCognome.insets = new Insets(0, 0, 5, 5);
-		gbc_lblCognome.gridx = 3;
-		gbc_lblCognome.gridy = 3;
-		contentPane.add(lblCognome, gbc_lblCognome);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		contentPane.add(lblCognome, gbc);
 		
 		textcognome = new JTextField();
-		GridBagConstraints gbc_textcognome = new GridBagConstraints();
-		gbc_textcognome.gridwidth = 2;
-		gbc_textcognome.insets = new Insets(0, 0, 5, 5);
-		gbc_textcognome.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textcognome.gridx = 4;
-		gbc_textcognome.gridy = 3;
-		contentPane.add(textcognome, gbc_textcognome);
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		contentPane.add(textcognome, gbc);
 		textcognome.setColumns(10);
 		
 		JLabel lblCodiceFiscale = new JLabel("Codice Fiscale");
-		GridBagConstraints gbc_lblCodiceFiscale = new GridBagConstraints();
-		gbc_lblCodiceFiscale.anchor = GridBagConstraints.EAST;
-		gbc_lblCodiceFiscale.insets = new Insets(0, 0, 5, 5);
-		gbc_lblCodiceFiscale.gridx = 3;
-		gbc_lblCodiceFiscale.gridy = 4;
-		contentPane.add(lblCodiceFiscale, gbc_lblCodiceFiscale);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		contentPane.add(lblCodiceFiscale, gbc);
 		
 		textcodice = new JTextField();
-		GridBagConstraints gbc_textcodice = new GridBagConstraints();
-		gbc_textcodice.gridwidth =2;
-		gbc_textcodice.insets = new Insets(0, 0, 5, 5);
-		gbc_textcodice.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textcodice.gridx = 4;
-		gbc_textcodice.gridy = 4;
-		contentPane.add(textcodice, gbc_textcodice);
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		contentPane.add(textcodice, gbc);
 		textcodice.setColumns(10);
 		
 		
 		JLabel lblCitt = new JLabel("Citta");
-		GridBagConstraints gbc_lblCitt = new GridBagConstraints();
-		gbc_lblCitt.anchor = GridBagConstraints.EAST;
-		gbc_lblCitt.insets = new Insets(0, 0, 5, 5);
-		gbc_lblCitt.gridx = 3;
-		gbc_lblCitt.gridy = 5;
-		contentPane.add(lblCitt, gbc_lblCitt);
+		
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.insets = new Insets(0, 0, 5, 5);
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		contentPane.add(lblCitt, gbc);
 		
 		textcitta = new JTextField();
-		GridBagConstraints gbc_textcitta = new GridBagConstraints();
-		gbc_textcitta.gridwidth = 2;
-		gbc_textcitta.insets = new Insets(0, 0, 5, 5);
-		gbc_textcitta.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textcitta.gridx = 4;
-		gbc_textcitta.gridy = 5;
-		contentPane.add(textcitta, gbc_textcitta);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		contentPane.add(textcitta, gbc);
 		textcitta.setColumns(10);
 		
 		JLabel lblVia = new JLabel("Via");
-		GridBagConstraints gbc_lblVia = new GridBagConstraints();
-		gbc_lblVia.anchor = GridBagConstraints.EAST;
-		gbc_lblVia.insets = new Insets(0, 0, 5, 5);
-		gbc_lblVia.gridx = 3;
-		gbc_lblVia.gridy = 6;
-		contentPane.add(lblVia, gbc_lblVia);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		contentPane.add(lblVia, gbc);
 		
 		textvia = new JTextField();
-		GridBagConstraints gbc_textvia = new GridBagConstraints();
-		gbc_textvia.gridwidth = 2;
-		gbc_textvia.insets = new Insets(0, 0, 5, 5);
-		gbc_textvia.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textvia.gridx = 4;
-		gbc_textvia.gridy = 6;
-		contentPane.add(textvia, gbc_textvia);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 6;
+		contentPane.add(textvia, gbc);
 		textvia.setColumns(10);
 		
 		JLabel lblNumeroCivico = new JLabel("Numero Civico");
-		GridBagConstraints gbc_lblNumeroCivico = new GridBagConstraints();
-		gbc_lblNumeroCivico.anchor = GridBagConstraints.EAST;
-		gbc_lblNumeroCivico.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNumeroCivico.gridx = 3;
-		gbc_lblNumeroCivico.gridy = 7;
-		contentPane.add(lblNumeroCivico, gbc_lblNumeroCivico);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		contentPane.add(lblNumeroCivico, gbc);
 		
 		textnumciv = new JTextField();
-		GridBagConstraints gbc_textnumciv = new GridBagConstraints();
-		gbc_textnumciv.gridwidth = 2;
-		gbc_textnumciv.insets = new Insets(0, 0, 5, 5);
-		gbc_textnumciv.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textnumciv.gridx = 4;
-		gbc_textnumciv.gridy = 7;
-		contentPane.add(textnumciv, gbc_textnumciv);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 7;
+		contentPane.add(textnumciv, gbc);
 		textnumciv.setColumns(10);
 		
 		JLabel lblDatiutenza = new JLabel("Dati di Accesso");
-		GridBagConstraints gbc_lblDatiutenza = new GridBagConstraints();
-		gbc_lblDatiutenza.gridwidth = 2;
-		gbc_lblDatiutenza.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDatiutenza.gridx = 4;
-		gbc_lblDatiutenza.gridy = 8;
-		contentPane.add(lblDatiutenza, gbc_lblDatiutenza);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 1;
+		gbc.gridy = 8;
+		contentPane.add(lblDatiutenza, gbc);
 		
 		
 		JLabel lblIndirizzoMail = new JLabel("Indirizzo mail");
-		GridBagConstraints gbc_lblIndirizzoMail = new GridBagConstraints();
-		gbc_lblIndirizzoMail.anchor = GridBagConstraints.EAST;
-		gbc_lblIndirizzoMail.insets = new Insets(0, 0, 5, 5);
-		gbc_lblIndirizzoMail.gridx = 3;
-		gbc_lblIndirizzoMail.gridy = 9;
-		contentPane.add(lblIndirizzoMail, gbc_lblIndirizzoMail);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 9;
+		contentPane.add(lblIndirizzoMail, gbc);
 		
 		textmail = new JTextField();
-		GridBagConstraints gbc_textmail = new GridBagConstraints();
-		gbc_textmail.gridwidth = 2;
-		gbc_textmail.insets = new Insets(0, 0, 5, 5);
-		gbc_textmail.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textmail.gridx = 4;
-		gbc_textmail.gridy = 9;
-		contentPane.add(textmail, gbc_textmail);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 9;
+		contentPane.add(textmail, gbc);
 		textmail.setColumns(10);
 		
 		JLabel lblNomeutente = new JLabel("Nome Utente");
-		GridBagConstraints gbc_lblNomeutente = new GridBagConstraints();
-		gbc_lblCognome.anchor = GridBagConstraints.EAST;
-		gbc_lblNomeutente.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNomeutente.gridx = 3;
-		gbc_lblNomeutente.gridy = 10;
-		contentPane.add(lblNomeutente, gbc_lblNomeutente);
-		
-		
-		
-		
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 10;
+		contentPane.add(lblNomeutente, gbc);
+			
+		textnomeutente = new JTextField();
+	    gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 10;
+		textnomeutente.setColumns(10);
+		contentPane.add(textnomeutente, gbc);
 		
 		JLabel inuso = new JLabel("Nome utente in uso");
-		GridBagConstraints gbc_inuso = new GridBagConstraints();
-		gbc_inuso.anchor = GridBagConstraints.EAST;
-		gbc_inuso.insets = new Insets(0, 0, 5, 5);
-		gbc_inuso.gridx = 3;
-		gbc_inuso.gridy = 11;
-		contentPane.add(inuso, gbc_inuso);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 2;
+		gbc.gridy = 10;
+		contentPane.add(inuso, gbc);
 		inuso.setVisible(false);
 		
-		textnomeutente = new JTextField();
-		GridBagConstraints gbc_textnomeutente = new GridBagConstraints();
-		gbc_textnomeutente.gridwidth = 2;
-		gbc_textnomeutente.insets = new Insets(0, 0, 5, 5);
-		gbc_textnomeutente.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textnomeutente.gridx = 4;
-		gbc_textnomeutente.gridy = 10;
-		textnomeutente.setColumns(10);
-		contentPane.add(textnomeutente, gbc_textnomeutente);
+	
 		
 		
 		JLabel lblPassword = new JLabel("Password");
-		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
-		gbc_lblPassword.anchor = GridBagConstraints.EAST;
-		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPassword.gridx = 3;
-		gbc_lblPassword.gridy = 12;
-		contentPane.add(lblPassword, gbc_lblPassword);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 11;
+		contentPane.add(lblPassword, gbc);
 		
 		pass = new JPasswordField();
-		GridBagConstraints gbc_passwordField1 = new GridBagConstraints();
-		gbc_passwordField1.gridwidth = 2;
-		gbc_passwordField1.insets = new Insets(0, 0, 5, 5);
-		gbc_passwordField1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_passwordField1.gridx = 4;
-		gbc_passwordField1.gridy = 12;
-		contentPane.add(pass, gbc_passwordField1);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 11;
 		pass.setColumns(10);
+		contentPane.add(pass, gbc);
+		
 		
 		
 		
 		JLabel lbl2Password = new JLabel("Ripeti Password");
-		GridBagConstraints gbc_lbl2Password = new GridBagConstraints();
-		gbc_lbl2Password.anchor = GridBagConstraints.EAST;
-		gbc_lbl2Password.insets = new Insets(0, 0, 5, 5);
-		gbc_lbl2Password.gridx = 3;
-		gbc_lbl2Password.gridy = 13;
-		contentPane.add(lbl2Password, gbc_lbl2Password);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridx = 0;
+		gbc.gridy = 12;
+		contentPane.add(lbl2Password, gbc);
 		
 		secondapass = new JPasswordField();
-		GridBagConstraints gbc_2passwordField = new GridBagConstraints();
-		gbc_2passwordField.gridwidth = 2;
-		gbc_2passwordField.insets = new Insets(0, 0, 5, 5);
-		gbc_2passwordField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_2passwordField.gridx = 4;
-		gbc_2passwordField.gridy = 13;
-		contentPane.add(secondapass, gbc_2passwordField);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 12;
 		secondapass.setColumns(10);
+		contentPane.add(secondapass, gbc);
+		
 		
 		JLabel lblTipoUtente = new JLabel("Tipo utente");
-		GridBagConstraints gbc_lblTipoUtente = new GridBagConstraints();
-		gbc_lblCognome.anchor = GridBagConstraints.EAST;
-		gbc_lblTipoUtente.insets = new Insets(0, 0, 5, 5);
-		gbc_lblTipoUtente.gridx = 3;
-		gbc_lblTipoUtente.gridy = 14;
-		contentPane.add(lblTipoUtente, gbc_lblTipoUtente);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridx = 0;
+		gbc.gridy = 13;
+		contentPane.add(lblTipoUtente, gbc);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Tesserato","Istruttore"}));
 		comboBox.setVisible(true);
 		comboBox.setEditable(false);
-		GridBagConstraints gbc_combotipoutente = new GridBagConstraints();
-		gbc_combotipoutente.gridwidth = 2;
-		gbc_combotipoutente.insets = new Insets(0, 0, 5, 5);
-		gbc_combotipoutente.fill = GridBagConstraints.HORIZONTAL;
-		gbc_combotipoutente.gridx = 4;
-		gbc_combotipoutente.gridy = 14;
-		contentPane.add(comboBox, gbc_combotipoutente);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.gridx = 1;
+		gbc.gridy = 13;
+		contentPane.add(comboBox, gbc);
 		
 	//	gbc_combotipoutente.setModel(new DefaultComboBoxModel(new String[] {"tesserato", "istruttore"}));
 		
@@ -387,12 +339,10 @@ public class FrameRegistrazione extends JFrame {
 		
 		
 		JButton btnRegistratiAlNostro = new JButton("Registrati al nostro portale!");
-		GridBagConstraints gbc_btnRegistratiAlNostro = new GridBagConstraints();
-		gbc_btnRegistratiAlNostro.gridwidth = 2;
-		gbc_btnRegistratiAlNostro.insets = new Insets(0, 0, 0, 5);
-		gbc_btnRegistratiAlNostro.gridx = 4;
-		gbc_btnRegistratiAlNostro.gridy = 16;
-		contentPane.add(btnRegistratiAlNostro, gbc_btnRegistratiAlNostro);
+		gbc.insets = new Insets(5, 0, 0, 10);
+		gbc.gridx = 1;
+		gbc.gridy = 14;
+		contentPane.add(btnRegistratiAlNostro, gbc);
 		btnRegistratiAlNostro.addActionListener(new ActionListener() {
 			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent arg0) {lblNome.setForeground(Color.BLACK);
