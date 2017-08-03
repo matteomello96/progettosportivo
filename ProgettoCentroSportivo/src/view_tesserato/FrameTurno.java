8607bed152c133f70f0433d8f9dfea7088e94f87
Claudio <Claudio@claudio>
2017-08-03 15:09:49 +0200

    03/08

--- ProgettoCentroSportivo/src/view_tesserato/FrameTurno.java
+++ ProgettoCentroSportivo/src/view_tesserato/FrameTurno.java
@@ -1,6 +1,7 @@
 package view_tesserato;
 
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -16,7 +17,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 import ClassiDaoTesserato.Invia_Turno_Dao;
 
@@ -48,7 +50,7 @@ public class FrameTurno extends JPanel {
 	public String ora;
 	public String giorno;
 	public JButton btnNewButton_1;
-
+    public JPanel contentPane;
 
 	public FrameTurno(String disciplina2, String livello2, String giorno2, String ora2) {
 		final DecimalFormat df = new DecimalFormat("0.00");
@@ -62,33 +64,41 @@ public class FrameTurno extends JPanel {
 		frame.setResizable(true);
 		frame.setAlwaysOnTop(true);
 		
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color (255,36,0));
+		contentPane.setLayout(new GridBagLayout());
 		
 		
 		
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{42, 0, 158, 72, 148, 75, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 36, 40, 0, 0, 0, 0, 119, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.setLayout(gridBagLayout);	
+		
+		JScrollPane scroll = new JScrollPane(contentPane);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setBounds(50, 30, 300, 50);			
+	    frame.getContentPane().add(scroll);
+	    
+	    
+	    GridBagConstraints gbc = new GridBagConstraints();
+	    
+	    
 		
 		JLabel lblNewLabel = new JLabel("Scelta turno");
 		lblNewLabel.setFont(new Font("Sylfaen", Font.BOLD, 25));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridwidth = 2;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 2;
-		gbc_lblNewLabel.gridy = 0;
-		frame.add(lblNewLabel, gbc_lblNewLabel);
+		
+		gbc.gridwidth = 2;
+		gbc.insets = new Insets(0, 0, 5, 5);
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		contentPane.add(lblNewLabel, gbc);
 	
 		JLabel lblNewLabel_1 = new JLabel("Sei iscritto a:");
 		lblNewLabel_1.setFont(new Font("Sylfaen", Font.PLAIN, 18));
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.gridwidth = 2;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 2;
-		frame.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		gbc.gridwidth = 2;
+		gbc.insets = new Insets(0, 0, 5, 5);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		contentPane.add(lblNewLabel_1, gbc);
 			
 		
 		
@@ -98,90 +108,83 @@ public class FrameTurno extends JPanel {
 		
 		 Combodis = new Combodis();
 		 Combodis.setEnabled(true);
-		 GridBagConstraints gbc_comboBox = new GridBagConstraints();
-			gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-			gbc_comboBox.gridx = 2;
-			gbc_comboBox.gridy = 2;
-			frame.add(Combodis, gbc_comboBox);
+		
+			gbc.insets = new Insets(0, 0, 5, 5);
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.gridx = 2;
+			gbc.gridy = 2;
+			contentPane.add(Combodis, gbc);
 		
 			JLabel lblNewLabel_2 = new JLabel("Livello:");
 			lblNewLabel_2.setFont(new Font("Sylfaen", Font.PLAIN, 18));
-			GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-			gbc_lblNewLabel_2.gridwidth = 2;
-			gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-			gbc_lblNewLabel_2.gridx = 0;
-			gbc_lblNewLabel_2.gridy = 3;
-			frame.add(lblNewLabel_2, gbc_lblNewLabel_2);
+			
+			gbc.gridwidth = 2;
+			gbc.insets = new Insets(0, 0, 5, 5);
+			gbc.gridx = 0;
+			gbc.gridy = 3;
+			contentPane.add(lblNewLabel_2, gbc);
 			
 			
 			
 			Combolivello = new combolivello(disciplina2);
 			 Combolivello.setEnabled(false);
-			 GridBagConstraints gbc_comboBox_3 = new GridBagConstraints();
-				gbc_comboBox_3.insets = new Insets(0, 0, 5, 5);
-				gbc_comboBox_3.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboBox_3.gridx = 2;
-				gbc_comboBox_3.gridy = 3;
-				frame.add(Combolivello, gbc_comboBox_3);
+			
+				gbc.insets = new Insets(0, 0, 5, 5);
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				gbc.gridx = 2;
+				gbc.gridy = 3;
+				contentPane.add(Combolivello, gbc);
 				
 				
 				JLabel lblNewLabel_4 = new JLabel("Orario:");
 				lblNewLabel_4.setFont(new Font("Sylfaen", Font.PLAIN, 18));
-				GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-				gbc_lblNewLabel_4.gridwidth = 2;
-				gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-				gbc_lblNewLabel_4.gridx = 0;
-				gbc_lblNewLabel_4.gridy = 5;
-				frame.add(lblNewLabel_4, gbc_lblNewLabel_4);
+			
+				gbc.gridwidth = 2;
+				gbc.insets = new Insets(0, 0, 5, 5);
+				gbc.gridx = 0;
+				gbc.gridy = 5;
+				contentPane.add(lblNewLabel_4, gbc);
 		
 		 Comboorario = new Comboorario(livello2,disciplina2,giorno2);
 		 Comboorario.setEnabled(false);
 		 
-		 GridBagConstraints gbc_comboBox_2 = new GridBagConstraints();
-			gbc_comboBox_2.insets = new Insets(0, 0, 5, 5);
-			gbc_comboBox_2.fill = GridBagConstraints.HORIZONTAL;
-			gbc_comboBox_2.gridx = 2;
-			gbc_comboBox_2.gridy = 5;
-			frame.add(Comboorario, gbc_comboBox_2);
-		// Comboorario.addItemListener(new ItemListener() {
-			//public void itemStateChanged(ItemEvent e) {
-			//	if(Comboorario.getSelectedIndex()<=0)
-				//	btnConferma.setEnabled(false);
-				//else btnConferma.setEnabled(true);
-			//}
-		//});
 		
+			gbc.insets = new Insets(0, 0, 5, 5);
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.gridx = 2;
+			gbc.gridy = 5;
+			contentPane.add(Comboorario, gbc);
+	
 			JLabel lblNewLabel_3 = new JLabel("Giorno:");
 			lblNewLabel_3.setFont(new Font("Sylfaen", Font.PLAIN, 18));
-			GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-			gbc_lblNewLabel_3.gridwidth = 2;
-			gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-			gbc_lblNewLabel_3.gridx = 0;
-			gbc_lblNewLabel_3.gridy = 4;
-			frame.add(lblNewLabel_3, gbc_lblNewLabel_3);
+			
+			gbc.gridwidth = 2;
+			gbc.insets = new Insets(0, 0, 5, 5);
+			gbc.gridx = 0;
+			gbc.gridy = 4;
+			contentPane.add(lblNewLabel_3, gbc);
 		
 		
 		
 		      Combogiorno = new Combogiorno(livello2,disciplina2);
 		      Combogiorno.setEnabled(false);
-		      GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-				gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
-				gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboBox_1.gridx = 2;
-				gbc_comboBox_1.gridy = 4;
-				frame.add(Combogiorno, gbc_comboBox_1);
+		    
+				gbc.insets = new Insets(0, 0, 5, 5);
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				gbc.gridx = 2;
+				gbc.gridy = 4;
+				contentPane.add(Combogiorno, gbc);
 				
 			
 
 			btnNewButton = new JButton("step successivo>>");
-			GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-			gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
-			gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-			gbc_btnNewButton.gridx = 2;
-			gbc_btnNewButton.gridy = 7;
+			
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(0, 0, 0, 5);
+			gbc.gridx = 2;
+			gbc.gridy = 7;
 			  btnNewButton.setEnabled(true);
-			frame.add(btnNewButton, gbc_btnNewButton);
+			contentPane.add(btnNewButton, gbc);
 			
 			
 		
